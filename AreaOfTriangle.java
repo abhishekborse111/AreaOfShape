ad51a756b192a455e7b1d4f4bca7aee60207cba4
abhishekborse111 <abhishekborse111@gmail.com>
2019-01-22 22:13:01 +0530

    Add files via upload

---
+++ AreaOfTriangle.java
@@ -0,0 +1,19 @@
+public class AreaOfTriangle{
+
+	public static void areaoftriangle(float base, float height){
+		System.out.println("Area of Triangle is:" +(base*height)/2);
+	}
+
+	public static void main(String[]args){
+		areaoftriangle(5.5f,2.2f);
+
+	}
+
+}
+	
+	
+
+
+
+
+
